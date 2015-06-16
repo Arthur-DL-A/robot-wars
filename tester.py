2c8b41ce1d4aa2cedba6c1b94422bbf8c774ea98
Zephyr12 <amartya.vadlamani@gmail.com>
2015-06-16 14:00:33 +0100

    added imports of client modules

--- tester.py
+++ tester.py
@@ -1,17 +1,26 @@
 import importlib
 
-class ServerModuleWrapper:
-    def __init__(self, playerModule):
-        self.playerModule = playerModule
+class PlayerWrapper:
+    def __init__(self, playerRobot):
+        self.playerRobot = playerRobot
         self.health = 20
         self.energy = 4 #currently used for testing complex patterns
     
-    def react(position):
-        pass
+    def act(self, tiles, boardSize, position):
+        attrDict = {
+                "health":self.health,
+                "energy":self.energy,
+                "tiles": tiles,
+                "boardsize": boardSize,
+                "position": position
+                }
+        return self.playerRobot.act(attrDict)
 
 
-player_one_module = importlib.import_module("player1")
-player_two_module = importlib.import_module("player2")
+player_one_module = importlib.import_module("ai-coward")
+player_one_robot = player_one_module.Robot()
+player_one = PlayerWrapper(player_one_robot)
+print(player_one.act([],(0,0), (0,0)))
 
 number_of_turns = 100
 
